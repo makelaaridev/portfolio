17511208d266909ab3d34e64bf1a2fa271181f27
Ari Mäkelä <brdzy.github@gmail.com>
2023-11-07 14:51:40 +0200

    Changes to be committed: 	deleted:    assets/aboutpic.png 	new file:   "assets/cv-ari-m\303\244kel\303\244.pdf" 	modified:   assets/project1.png 	deleted:    assets/resume-example.pdf 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
