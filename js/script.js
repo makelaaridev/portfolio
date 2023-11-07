ce32ce8b384e618b467f10718c0bc2fd487c804a
Ari <107745306+makelaaridev@users.noreply.github.com>
2023-11-07 17:41:03 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,3 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
